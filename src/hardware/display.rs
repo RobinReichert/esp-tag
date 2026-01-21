09a84a17629847a583c4b9337448aa21d8224b26
Robin Reichert <robin@reichert-online.eu>
2026-01-22 00:58:49 +0100

    implemented a basic display struct and shared bus

---
+++ src/hardware/display.rs
@@ -0,0 +1,58 @@
+use ssd1306::{mode::BufferedGraphicsModeAsync, prelude::*, I2CDisplayInterface, Ssd1306Async};
+use embedded_graphics::{
+    image::{Image, ImageRaw},
+    mono_font::{ascii::FONT_6X10, iso_8859_2::FONT_9X18, MonoTextStyle, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::{Point, *},
+    text::{Alignment, Text, Baseline},
+};
+use embedded_hal_async::i2c::I2c;
+use crate::hardware::{bmp, error::DisplayError};
+
+const WIDTH: usize = 128;
+const HEIGHT: usize = 32;
+const BUFFER_SIZE: usize = WIDTH * HEIGHT / 8;
+
+pub struct Display<I2C> {
+    display: Ssd1306Async<I2CInterface<I2C>, DisplaySize128x32, BufferedGraphicsModeAsync<DisplaySize128x32>>
+}
+
+impl<I2C> Display<I2C> 
+where I2C: I2c
+{
+    pub fn new(i2c: I2C) -> Self {
+        let interface = I2CDisplayInterface::new(i2c);
+        let display = Ssd1306Async::new(interface, DisplaySize128x32, DisplayRotation::Rotate0).into_buffered_graphics_mode();
+        return Display { display }
+    }
+
+    pub async fn init(&mut self) -> Result<(), DisplayError> {
+        self.display.init().await.map_err(|_| DisplayError::InitError)
+    }
+
+    pub async fn show_logo(&mut self) -> Result<(), DisplayError> {
+        let raw_image = ImageRaw::<BinaryColor>::new(&bmp::LOGO_BITMAP, 128);
+        let image = Image::new(&raw_image, Point::new(8, 0));
+        image.draw(&mut self.display).map_err(|_| DisplayError::DrawError)?;
+        self.display.flush().await.map_err(|_| DisplayError::FlushError)
+    }
+
+    pub async fn show_center_text(&mut self, text: &str) -> Result<(), DisplayError> {
+        self.display.clear(BinaryColor::Off).map_err(|_| DisplayError::ClearError)?;
+        Text::with_alignment(text, Point::new(64, 20), MonoTextStyle::new(&FONT_9X18, BinaryColor::On), Alignment::Center)
+            .draw(&mut self.display).map_err(|_| DisplayError::DrawError)?;
+        self.display.flush().await.map_err(|_| DisplayError::FlushError)
+    }
+
+    pub async fn show_text_at(&mut self, text: &str, x: i32, y: i32) -> Result<(), DisplayError> {
+        self.display.clear(BinaryColor::Off).map_err(|_| DisplayError::ClearError)?;
+        Text::with_baseline(text, Point::new(x, y), MonoTextStyle::new(&FONT_9X18, BinaryColor::On), Baseline::Top)
+            .draw(&mut self.display).map_err(|_| DisplayError::DrawError)?;
+        self.display.flush().await.map_err(|_| DisplayError::FlushError)
+    }
+
+    pub async fn clear(&mut self) -> Result<(), DisplayError> {
+        self.display.clear(BinaryColor::Off).map_err(|_| DisplayError::ClearError)?;
+        self.display.flush().await.map_err(|_| DisplayError::FlushError)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,17 @@ mod hardware;
 mod logic;
 
 use embassy_executor::Spawner;
-use embassy_sync::channel::Channel;
-use embassy_sync::mutex::Mutex;
-use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
-use embassy_time::{Duration, Ticker};
+use embassy_time::{Duration, Timer};
 use esp_alloc as _;
 use esp_backtrace as _;
 use esp_hal::{
-    clock::CpuClock, interrupt::software::SoftwareInterruptControl, timer::timg::TimerGroup,
+    time::Rate, clock::CpuClock, interrupt::software::SoftwareInterruptControl, timer::timg::TimerGroup,
 };
 use esp_println::println;
 use esp_radio::{Controller, esp_now::BROADCAST_ADDRESS};
+use embedded_hal_bus::{i2c::{self, AtomicDevice}, util::AtomicCell};
 
-use crate::logic::{mesh, message, node::Node, tree::Tree};
+use crate::{hardware::{display::Display, shared_bus::{SharedBus, SharedBusInterface}}, logic::{mesh, message, node::Node, tree::Tree}};
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -40,52 +38,34 @@ async fn main(spawner: Spawner) -> ! {
     esp_rtos::start(timg0.timer0, sw_int.software_interrupt0);
 
     let esp_radio_ctrl = &*mk_static!(Controller<'static>, esp_radio::init().unwrap());
-
     let wifi = peripherals.WIFI;
     let (mut controller, interfaces) =
         esp_radio::wifi::new(&esp_radio_ctrl, wifi, Default::default()).unwrap();
     controller.set_mode(esp_radio::wifi::WifiMode::Sta).unwrap();
     controller.start().unwrap();
 
-    let esp_now = interfaces.esp_now;
-    esp_now.set_channel(11).unwrap();
-    esp_println::println!("esp-now version {}", esp_now.version().unwrap());
-    let (_, sender, receiver) = esp_now.split();
-    loop {}
-    /*
-        let joined: Signal<NoopRawMutex, bool> = Signal::new();
-        let joined = mk_static!(Signal<NoopRawMutex, bool>, joined);
-        let leader: Signal<NoopRawMutex, bool> = Signal::new();
-        let leader = mk_static!(Signal<NoopRawMutex, bool>, leader);
-        let send_queue: Channel<NoopRawMutex, message::SendMessage, 16> = Channel::new();
-        let receive_queue: Channel<NoopRawMutex, message::ReceiveMessage, 16> = Channel::new();
-        let return_queue: Channel<NoopRawMutex, message::MessageData, 16> = Channel::new();
-        let leader_queue: Channel<NoopRawMutex, message::ReceiveMessage, 16> = Channel::new();
-        let leader_queue = mk_static!(Channel<NoopRawMutex, message::ReceiveMessage, 16>, leader_queue);
-        let send_queue = mk_static!(Channel<NoopRawMutex, message::SendMessage, 16>, send_queue);
-        let receive_queue =
-        mk_static!(Channel<NoopRawMutex, message::ReceiveMessage, 16>, receive_queue);
-        let return_queue = mk_static!(Channel<NoopRawMutex, message::MessageData, 16>, return_queue);
-        let own_node = Node::new(BROADCAST_ADDRESS);
-        let route: Mutex<NoopRawMutex, Tree> = Mutex::new(unwrap_print!(Tree::new(own_node)));
-        let route = mk_static!(Mutex<NoopRawMutex, Tree>, route);
-        let mesh = unwrap_print!(Mesh::new(
-            spawner,
-            route,
-            receiver,
-            sender
-        ));
-        match mesh.send(b"hallo123test", Node::new([1, 2, 3, 4, 5, 6])) {
-            Ok(_) => (),
-            Err(e) => println!("error:  {}", e),
-        }
+    let i2c_bus = esp_hal::i2c::master::I2c::new(
+        peripherals.I2C0,
+        esp_hal::i2c::master::Config::default().with_frequency(Rate::from_khz(400)),
+    )
+        .unwrap()
+        .with_scl(peripherals.GPIO9)
+        .with_sda(peripherals.GPIO8)
+        .into_async();
 
-        let mut ticker = Ticker::every(Duration::from_millis(500));
-        loop {
-            if mesh.has_message() {
-                println!("{:?}", mesh.get_message().expect("failed to get message"));
-            }
-            ticker.next().await;
-        }
-    */
+    let shared_bus = SharedBus::new(i2c_bus);
+
+    let mut display = Display::new(SharedBusInterface::new(&shared_bus));
+    display.init().await;
+    unwrap_print!(display.show_center_text("Welcome").await);
+    Timer::after(Duration::from_millis(500)).await;
+    unwrap_print!(display.clear().await);
+    Timer::after(Duration::from_millis(100)).await;
+    unwrap_print!(display.show_center_text("to").await);
+    Timer::after(Duration::from_millis(500)).await;
+    unwrap_print!(display.clear().await);
+    Timer::after(Duration::from_millis(100)).await;
+    unwrap_print!(display.show_logo().await);
+
+    loop {}
 }
